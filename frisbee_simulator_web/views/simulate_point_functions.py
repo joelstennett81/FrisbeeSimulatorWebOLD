d0edf065b39419783a78027343f425920af96c6d
Joel Stennett <joelstennett17@gmail.com>
2023-12-03 02:21:04 -0600

    Update simulate_point_functions.py

--- frisbee_simulator_web/views/simulate_point_functions.py
+++ frisbee_simulator_web/views/simulate_point_functions.py
@@ -4,6 +4,7 @@ import random
 class PointSimulation:
     def __init__(self, gameSimulation):
         super().__init__()
+        self.throwStartingProbability = None
         self.gameSimulation = gameSimulation
         self.teamOne = self.gameSimulation.teamOne
         self.teamTwo = self.gameSimulation.teamTwo
@@ -165,9 +166,7 @@ class PointSimulation:
             self.deepHuckReceiverOptionThree = 5
             self.deepHuckReceiverOptionFour = 0
 
-    def simulate_throw(self, throwStartingProbability):
-        self.playerWithDisc == self.sevenOnFieldForTeamOne.all()[self.throwerNumber]
-        self.playerGuardingDisc = self.sevenOnFieldForTeamTwo.all()[self.throwerNumber]
+    def simulate_swing_throw(self):
         if self.throwChoice == 'swing':
             self.receiverOptions = [self.sevenOnFieldForTeamOne.all()[self.swingReceiverOptionOne],
                                     self.sevenOnFieldForTeamOne.all()[self.swingReceiverOptionTwo]]
@@ -179,7 +178,7 @@ class PointSimulation:
             self.probabilityThrowIsCompleted = (random.randint(1, 100) +
                                                 self.playerWithDisc.under_throw_offense + self.playerBeingThrownTo.under_cut_offense
                                                 - self.playerGuardingDisc.handle_mark_defense - self.playerGuardingPlayerBeingThrownTo.under_cut_defense)
-            if self.probabilityThrowIsCompleted < throwStartingProbability:
+            if self.probabilityThrowIsCompleted < self.throwStartingProbability:
                 randomYardsThrownOption = range(5, 15)
                 randomChoice = random.randint(1, len(randomYardsThrownOption))
                 self.discLocation += randomYardsThrownOption[randomChoice]
@@ -200,132 +199,140 @@ class PointSimulation:
                 self.teamWithDisc = self.teamTwo
                 self.playerWithDisc = self.playerGuardingPlayerBeingThrownTo
                 self.playerGuardingDisc = self.defenderOptions[randomReceiver]
-        elif self.throwChoice == 'under':
-            self.receiverOptions = [self.sevenOnFieldForTeamOne.all()[self.underReceiverOptionOne],
-                                    self.sevenOnFieldForTeamOne.all()[self.underReceiverOptionTwo]]
-            self.defenderOptions = [self.sevenOnFieldForTeamTwo.all()[self.underReceiverOptionOne],
-                                    self.sevenOnFieldForTeamTwo.all()[self.underReceiverOptionTwo]]
-            randomReceiver = random.randint(0, 1)
-            self.playerBeingThrownTo = self.receiverOptions[randomReceiver]
-            self.playerGuardingPlayerBeingThrownTo = self.defenderOptions[randomReceiver]
-            self.probabilityThrowIsCompleted = (random.randint(1, 100) +
-                                                self.playerWithDisc.under_throw_offense + self.playerBeingThrownTo.under_cut_offense
-                                                - self.playerGuardingDisc.handle_mark_defense - self.playerGuardingPlayerBeingThrownTo.under_cut_defense)
-            # if random number is less than 90, its a completion, else its a turnover
-            if self.probabilityThrowIsCompleted < throwStartingProbability:
-                randomYardsThrownOption = range(5, 15)
-                randomChoice = random.randint(1, len(randomYardsThrownOption))
-                self.discLocation += randomYardsThrownOption[randomChoice]
-                if self.discLocation < -20:
-                    # turnover, disc goes to goal line at location 0 and team 2 has disc
-                    self.teamWithDisc = self.teamTwo
-                    self.playerWithDisc = self.playerGuardingPlayerBeingThrownTo
-                    self.playerGuardingDisc = self.defenderOptions[randomReceiver]
-                elif 70 < self.discLocation < 90:
-                    self.assistThrower = self.playerWithDisc
-                    self.goalThrower = self.playerBeingThrownTo
-                    self.pointWinner = self.teamOne
-                    self.pointOver = True
-                else:
-                    self.playerWithDisc = self.playerGuardingPlayerBeingThrownTo
-                    self.playerGuardingDisc = self.playerGuardingPlayerBeingThrownTo
-            else:
+
+    def simulate_under_throw(self):
+        self.receiverOptions = [self.sevenOnFieldForTeamOne.all()[self.underReceiverOptionOne],
+                                self.sevenOnFieldForTeamOne.all()[self.underReceiverOptionTwo]]
+        self.defenderOptions = [self.sevenOnFieldForTeamTwo.all()[self.underReceiverOptionOne],
+                                self.sevenOnFieldForTeamTwo.all()[self.underReceiverOptionTwo]]
+        randomReceiver = random.randint(0, 1)
+        self.playerBeingThrownTo = self.receiverOptions[randomReceiver]
+        self.playerGuardingPlayerBeingThrownTo = self.defenderOptions[randomReceiver]
+        self.probabilityThrowIsCompleted = (random.randint(1, 100) +
+                                            self.playerWithDisc.under_throw_offense + self.playerBeingThrownTo.under_cut_offense
+                                            - self.playerGuardingDisc.handle_mark_defense - self.playerGuardingPlayerBeingThrownTo.under_cut_defense)
+        # if random number is less than 90, its a completion, else its a turnover
+        if self.probabilityThrowIsCompleted < self.throwStartingProbability:
+            randomYardsThrownOption = range(5, 15)
+            randomChoice = random.randint(1, len(randomYardsThrownOption))
+            self.discLocation += randomYardsThrownOption[randomChoice]
+            if self.discLocation < -20:
+                # turnover, disc goes to goal line at location 0 and team 2 has disc
                 self.teamWithDisc = self.teamTwo
                 self.playerWithDisc = self.playerGuardingPlayerBeingThrownTo
-                self.playerGuardingDisc = self.receiverOptions[randomReceiver]
-        elif self.throwChoice == 'short_huck':
-            self.receiverOptions = [self.sevenOnFieldForTeamOne.all()[self.shortHuckReceiverOptionOne],
-                                    self.sevenOnFieldForTeamOne.all()[self.shortHuckReceiverOptionTwo]]
-            self.defenderOptions = [self.sevenOnFieldForTeamTwo.all()[self.shortHuckReceiverOptionOne],
-                                    self.sevenOnFieldForTeamTwo.all()[self.shortHuckReceiverOptionTwo]]
-            randomReceiver = random.randint(0, 1)
-            self.playerBeingThrownTo = self.receiverOptions[randomReceiver]
-            self.playerGuardingPlayerBeingThrownTo = self.defenderOptions[randomReceiver]
-            self.probabilityThrowIsCompleted = (random.randint(1, 100) +
-                                                self.playerWithDisc.under_throw_offense + self.playerBeingThrownTo.under_cut_offense
-                                                - self.playerGuardingDisc.handle_mark_defense - self.playerGuardingPlayerBeingThrownTo.under_cut_defense)
-            # if random number is less than 90, its a completion, else its a turnover
-            if self.probabilityThrowIsCompleted < throwStartingProbability:
-                randomYardsThrownOption = range(15, 30)
-                randomChoice = random.randint(1, len(randomYardsThrownOption))
-                self.discLocation += randomYardsThrownOption[randomChoice]
-                if self.discLocation < -20:
-                    # turnover, disc goes to goal line at location 0 and team 2 has disc
-                    self.teamWithDisc = self.teamTwo
-                    self.playerWithDisc = self.playerGuardingPlayerBeingThrownTo
-                    self.playerGuardingDisc = self.defenderOptions[randomReceiver]
-                elif 70 < self.discLocation < 90:
-                    self.assistThrower = self.playerWithDisc
-                    self.goalThrower = self.playerBeingThrownTo
-                    self.pointWinner = self.teamOne
-                    self.pointOver = True
-                else:
-                    self.playerWithDisc = self.playerGuardingPlayerBeingThrownTo
-                    self.playerGuardingDisc = self.playerGuardingPlayerBeingThrownTo
+                self.playerGuardingDisc = self.defenderOptions[randomReceiver]
+            elif 70 < self.discLocation < 90:
+                self.assistThrower = self.playerWithDisc
+                self.goalThrower = self.playerBeingThrownTo
+                self.pointWinner = self.teamOne
+                self.pointOver = True
             else:
+                self.playerWithDisc = self.playerGuardingPlayerBeingThrownTo
+                self.playerGuardingDisc = self.playerGuardingPlayerBeingThrownTo
+        else:
+            self.teamWithDisc = self.teamTwo
+            self.playerWithDisc = self.playerGuardingPlayerBeingThrownTo
+            self.playerGuardingDisc = self.receiverOptions[randomReceiver]
+
+    def simulate_short_huck_throw(self):
+        self.receiverOptions = [self.sevenOnFieldForTeamOne.all()[self.shortHuckReceiverOptionOne],
+                                self.sevenOnFieldForTeamOne.all()[self.shortHuckReceiverOptionTwo]]
+        self.defenderOptions = [self.sevenOnFieldForTeamTwo.all()[self.shortHuckReceiverOptionOne],
+                                self.sevenOnFieldForTeamTwo.all()[self.shortHuckReceiverOptionTwo]]
+        randomReceiver = random.randint(0, 1)
+        self.playerBeingThrownTo = self.receiverOptions[randomReceiver]
+        self.playerGuardingPlayerBeingThrownTo = self.defenderOptions[randomReceiver]
+        self.probabilityThrowIsCompleted = (random.randint(1, 100) +
+                                            self.playerWithDisc.under_throw_offense + self.playerBeingThrownTo.under_cut_offense
+                                            - self.playerGuardingDisc.handle_mark_defense - self.playerGuardingPlayerBeingThrownTo.under_cut_defense)
+        # if random number is less than 90, its a completion, else its a turnover
+        if self.probabilityThrowIsCompleted < self.throwStartingProbability:
+            randomYardsThrownOption = range(15, 30)
+            randomChoice = random.randint(1, len(randomYardsThrownOption))
+            self.discLocation += randomYardsThrownOption[randomChoice]
+            if self.discLocation < -20:
+                # turnover, disc goes to goal line at location 0 and team 2 has disc
                 self.teamWithDisc = self.teamTwo
                 self.playerWithDisc = self.playerGuardingPlayerBeingThrownTo
-                self.playerGuardingDisc = self.receiverOptions[randomReceiver]
-        elif self.throwChoice == 'deep_huck':
-            self.receiverOptions = [self.sevenOnFieldForTeamOne.all()[self.deepHuckReceiverOptionOne],
-                                    self.sevenOnFieldForTeamOne.all()[self.deepHuckReceiverOptionTwo],
-                                    self.sevenOnFieldForTeamOne.all()[self.deepHuckReceiverOptionThree],
-                                    self.sevenOnFieldForTeamOne.all()[self.deepHuckReceiverOptionFour]]
-            self.defenderOptions = [self.sevenOnFieldForTeamTwo.all()[self.deepHuckReceiverOptionOne],
-                                    self.sevenOnFieldForTeamTwo.all()[self.deepHuckReceiverOptionTwo],
-                                    self.sevenOnFieldForTeamTwo.all()[self.deepHuckReceiverOptionThree],
-                                    self.sevenOnFieldForTeamTwo.all()[self.deepHuckReceiverOptionFour]]
-            randomReceiver = random.randint(0, 3)
-            self.playerBeingThrownTo = self.receiverOptions[randomReceiver]
-            self.playerGuardingPlayerBeingThrownTo = self.defenderOptions[randomReceiver]
-            self.probabilityThrowIsCompleted = (random.randint(1, 100) +
-                                                self.playerWithDisc.under_throw_offense + self.playerBeingThrownTo.under_cut_offense
-                                                - self.playerGuardingDisc.handle_mark_defense - self.playerGuardingPlayerBeingThrownTo.under_cut_defense)
-            # if random number is less than 90, its a completion, else its a turnover
-            if self.probabilityThrowIsCompleted < throwStartingProbability:
-                randomYardsThrownOption = range(15, 30)
-                randomChoice = random.randint(1, len(randomYardsThrownOption))
-                self.discLocation += randomYardsThrownOption[randomChoice]
-                if self.discLocation < -20:
-                    # turnover, disc goes to goal line at location 0 and team 2 has disc
-                    self.teamWithDisc = self.teamTwo
-                    self.playerWithDisc = self.playerGuardingPlayerBeingThrownTo
-                    self.playerGuardingDisc = self.defenderOptions[randomReceiver]
-                elif 70 < self.discLocation < 90:
-                    self.assistThrower = self.playerWithDisc
-                    self.goalThrower = self.playerBeingThrownTo
-                    self.pointWinner = self.teamOne
-                    self.pointOver = True
-                else:
-                    self.playerWithDisc = self.playerGuardingPlayerBeingThrownTo
-                    self.playerGuardingDisc = self.playerGuardingPlayerBeingThrownTo
+                self.playerGuardingDisc = self.defenderOptions[randomReceiver]
+            elif 70 < self.discLocation < 90:
+                self.assistThrower = self.playerWithDisc
+                self.goalThrower = self.playerBeingThrownTo
+                self.pointWinner = self.teamOne
+                self.pointOver = True
             else:
+                self.playerWithDisc = self.playerGuardingPlayerBeingThrownTo
+                self.playerGuardingDisc = self.playerGuardingPlayerBeingThrownTo
+        else:
+            self.teamWithDisc = self.teamTwo
+            self.playerWithDisc = self.playerGuardingPlayerBeingThrownTo
+            self.playerGuardingDisc = self.receiverOptions[randomReceiver]
+
+    def simulate_deep_huck_throw(self):
+        self.receiverOptions = [self.sevenOnFieldForTeamOne.all()[self.deepHuckReceiverOptionOne],
+                                self.sevenOnFieldForTeamOne.all()[self.deepHuckReceiverOptionTwo],
+                                self.sevenOnFieldForTeamOne.all()[self.deepHuckReceiverOptionThree],
+                                self.sevenOnFieldForTeamOne.all()[self.deepHuckReceiverOptionFour]]
+        self.defenderOptions = [self.sevenOnFieldForTeamTwo.all()[self.deepHuckReceiverOptionOne],
+                                self.sevenOnFieldForTeamTwo.all()[self.deepHuckReceiverOptionTwo],
+                                self.sevenOnFieldForTeamTwo.all()[self.deepHuckReceiverOptionThree],
+                                self.sevenOnFieldForTeamTwo.all()[self.deepHuckReceiverOptionFour]]
+        randomReceiver = random.randint(0, 3)
+        self.playerBeingThrownTo = self.receiverOptions[randomReceiver]
+        self.playerGuardingPlayerBeingThrownTo = self.defenderOptions[randomReceiver]
+        self.probabilityThrowIsCompleted = (random.randint(1, 100) +
+                                            self.playerWithDisc.under_throw_offense + self.playerBeingThrownTo.under_cut_offense
+                                            - self.playerGuardingDisc.handle_mark_defense - self.playerGuardingPlayerBeingThrownTo.under_cut_defense)
+        # if random number is less than 90, its a completion, else its a turnover
+        if self.probabilityThrowIsCompleted < self.throwStartingProbability:
+            randomYardsThrownOption = range(15, 30)
+            randomChoice = random.randint(1, len(randomYardsThrownOption))
+            self.discLocation += randomYardsThrownOption[randomChoice]
+            if self.discLocation < -20:
+                # turnover, disc goes to goal line at location 0 and team 2 has disc
                 self.teamWithDisc = self.teamTwo
                 self.playerWithDisc = self.playerGuardingPlayerBeingThrownTo
-                self.playerGuardingDisc = self.receiverOptions[randomReceiver]
+                self.playerGuardingDisc = self.defenderOptions[randomReceiver]
+            elif 70 < self.discLocation < 90:
+                self.assistThrower = self.playerWithDisc
+                self.goalThrower = self.playerBeingThrownTo
+                self.pointWinner = self.teamOne
+                self.pointOver = True
+            else:
+                self.playerWithDisc = self.playerGuardingPlayerBeingThrownTo
+                self.playerGuardingDisc = self.playerGuardingPlayerBeingThrownTo
+        else:
+            self.teamWithDisc = self.teamTwo
+            self.playerWithDisc = self.playerGuardingPlayerBeingThrownTo
+            self.playerGuardingDisc = self.receiverOptions[randomReceiver]
 
+    def simulate_play_until_score(self):
+        # four pass options:
+        # 1: swing for -5 to +5 yards (random number between 0-3
+        # 2: under for 5 to 15 yards (random number between 4-6
+        # 3: short huck for 15-30 yards (random number between 7-8)
+        # 4: deep huck for 31-70 yards (cant go thru end of end zone) (random number == 9-10
 
-def simulate_play_until_score(self):
-    # four pass options:
-    # 1: swing for -5 to +5 yards (random number between 0-3
-    # 2: under for 5 to 15 yards (random number between 4-6
-    # 3: short huck for 15-30 yards (random number between 7-8)
-    # 4: deep huck for 31-70 yards (cant go thru end of end zone) (random number == 9-10
-
-    # Probability of Turnover
-    while self.pointOver == False:
-        if self.teamWithDisc == self.teamOne:
-            # determine what throw is going to be thrown
-            random_number = random.randint(0, 10)
-            if random_number in [0, 1, 2, 3]:
-                self.throwChoice = 'swing'
-                self.simulate_throw(90)
-            elif random_number in [4, 5, 6]:
-                self.throwChoice = 'under'
-                self.simulate_throw(80)
-            elif random_number in [7, 8]:
-                self.throwChoice = 'short_huck'
-                self.simulate_throw(65)
-            elif random_number in [9, 10]:
-                self.throwChoice = 'deep_huck'
-                self.simulate_throw(55)
+        # Probability of Turnover
+        while self.pointOver == False:
+            if self.teamWithDisc == self.teamOne:
+                self.playerWithDisc == self.sevenOnFieldForTeamOne.all()[self.throwerNumber]
+                self.playerGuardingDisc = self.sevenOnFieldForTeamTwo.all()[self.throwerNumber]
+                # determine what throw is going to be thrown
+                random_number = random.randint(0, 10)
+                if random_number in [0, 1, 2, 3]:
+                    self.throwChoice = 'swing'
+                    self.throwStartingProbability = 90
+                    self.simulate_swing_throw()
+                elif random_number in [4, 5, 6]:
+                    self.throwChoice = 'under'
+                    self.throwStartingProbability = 80
+                    self.simulate_under_throw()
+                elif random_number in [7, 8]:
+                    self.throwChoice = 'short_huck'
+                    self.throwStartingProbability = 65
+                    self.simulate_short_huck_throw()
+                elif random_number in [9, 10]:
+                    self.throwChoice = 'deep_huck'
+                    self.throwStartingProbability = 55
+                    self.simulate_deep_huck_throw()
